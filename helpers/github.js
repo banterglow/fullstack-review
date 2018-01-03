72232dc69ea7eca6d521594ae05daeba97414f84
wongal5 <a.wong9970@gmail.com>
2018-01-03 10:44:22 -0800

    removed reference to config.js and added environment variable for api key

--- helpers/github.js
+++ helpers/github.js
@@ -1,12 +1,11 @@
 const request = require('request-promise');
-const config = require(__dirname + '/../config.js');
 
 let getReposByUsername = (username) => {
   let options = {
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${GITHUBTOKEN}`
     }
   };
   
